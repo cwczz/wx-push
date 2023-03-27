69dee413cc20cbd3655e9618f45ec60ffd5582cd
乔碧萝爷爱了 <49864505+cwczz@users.noreply.github.com>
2023-03-27 14:15:34 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -2,7 +2,7 @@ module.exports = {
   appid: "wxcf9462c972fde835",
   appsecret: "83003afb2384b5ddb40373ff0b814508",
   template_id: "jbJR7fbbVycGUudQ66tkcknfQ_2b0vX7u4bPyUloE8Y",
-  touser: "orltV6hdlAHsePcsCCSJ_EzuMLpc",
+  touser: "orltV6lv8R_cxUM4YwXeGqHVhKqk",
 
   key: "201c93c2e2b34bdeecdbf9f5265eb84e",
   city: "深圳",
